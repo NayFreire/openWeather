56f9ace3b166b3b8b75f4d8a7155e2c92594563b
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-04-24 18:20:44 -0300

    Consegui mostrar o nome da cidade na página

--- getData.js
+++ getData.js
@@ -1,22 +1,23 @@
-const dados = require('./index') //Importando os dados do index.js
 const express = require('express')
 const app = express()
+const dados = require('./index') //Importando os dados do index.js
+var response = []
 
 app.use('/', (req, res) => {
     retornoApi = dados.getData(req.body.cityName)
     
     retornoApi.then((resposta) => {
         //res.send(resposta.data)
-        const response = {
+        response = {
             dados: {
                 cidade: resposta.data.name,
                 pais: resposta.data.sys.country,
                 temperatura: resposta.data.main.temp,
                 umidade: resposta.data.main.humidity,
-                clima: resposta.data.weather
+                climaPrincipal: resposta.data.weather.main,
+                climaDescricao: resposta.data.weather.description,
             }    
         }
-        module.exports = response
         // return res.status(200).send(response)
     }).catch((err) => { //Em caso de erro...
         if(err){
@@ -29,3 +30,5 @@ app.use('/', (req, res) => {
     })
     //* Eu preciso do .then, pois dados se torna uma promise. Como a requisição de dados da api é uma operação assíncrona.  Isto permite que métodos assíncronos retornem valores como métodos síncronos: ao invés do valor final, o método assíncrono retorna uma promessa ao valor em algum momento no futuro.
 })
+
+module.exports = response
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const dados = require('./getData')
+const dados = require('./index') //Importando os dados do index.js
 const handlebars = require('express-handlebars')
 
 const Client = require('pg').Client
@@ -25,8 +25,41 @@ app.set('view engine', 'handlebars')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+// app.use('/', (req, res) => {
+    
+// })
+
 app.get('/', (req, res) => {
-    res.render('index', {data: dados})
+    var response = []
+    retornoApi = dados.getData(req.body.cityName)
+    
+    retornoApi.then((resposta) => {
+        //res.send(resposta.data)
+        response = {
+            dados: {
+                cidade: resposta.data.name,
+                pais: resposta.data.sys.country,
+                temperatura: resposta.data.main.temp,
+                umidade: resposta.data.main.humidity,
+                climaPrincipal: resposta.data.weather[0].main,
+                climaDesc: resposta.data.weather[0].description
+            }
+        }
+
+        // return res.status(200).send(response)
+        console.log(response)
+        res.render('index', {data: response})
+    }).catch((err) => { //Em caso de erro...
+        if(err){
+            console.error(err) //mostre o erro
+            return res.send({
+                message: 'Erro ao buscar cidade',
+                error: err
+            })
+        }
+    })
+    //* Eu preciso do .then, pois dados se torna uma promise. Como a requisição de dados da api é uma operação assíncrona.  Isto permite que métodos assíncronos retornem valores como métodos síncronos: ao invés do valor final, o método assíncrono retorna uma promessa ao valor em algum momento no futuro.
+    
 })
 
 app.listen(5050, () => {
