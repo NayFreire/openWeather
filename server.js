16fd5b04ecf97a59b0fcd24d2d4a2bf7c55b99c3
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-04-25 15:41:18 -0300

    Tentativa com o sequelize

--- server.js
+++ server.js
@@ -3,34 +3,6 @@ const app = express()
 const dados = require('./index') //Importando os dados do index.js
 const handlebars = require('express-handlebars')
 
-const Client = require('pg').Client
-const { response } = require('express')
-const cliente = new Client({
-                            user: process.env.PGUSER,
-                            password: process.env.PGPASSW,
-                            host: process.env.PGHOST,
-                            port: process.env.PGPORT,
-                            database: process.env.PGDATABASE
-})
-
-async function getCidades(){
-    try{
-        await cliente.connect()
-        const resultado = await cliente.query('SELECT * FROM cidades')
-        console.log('-------------Resultado do banco---------------')
-        console.log(resultado.rows)
-        console.log('----------------------------------------------')
-        return resultado.rows
-    }
-    catch (ex){
-        console.log("Ocorreu um erro: \n" + ex)
-    }
-    finally{
-        await cliente.end()
-    }
-}
-
-
 //Configurando o template engine do express
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -39,32 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("."));
 
-// app.use('/', (req, res) => {
-    
-// })
-
 app.get('/', (req, res) => {
-    var response = {}
-    var dados = getCidades()
-    dados.then((resposta) => {
-        response = {
-            //!TODO: USAR MAP PARA MOSTRAR MAIS DE UM REGISTRO NO BANCO DE DADOS
-            dados: {
-                encontrada: true,
-                cidade: resposta[0].nome,
-                pais: resposta[0].pais,
-                temperatura: resposta[0].temperatura,
-                umidade: resposta[0].umidade,
-                climaPrincipal: resposta[0].climaprincipal,
-                climaDesc: resposta[0].climadescricao,
-                numBuscas: resposta[0].numbuscas
-            }
-        }
-        console.log(response)
-        res.render('index', {data: response})
-    }).catch((err) => {
-        console.log(err)
-    })
+    res.render('index', {data: response})
 })
 
 app.post('/', (req, res) => {
